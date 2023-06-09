33751470b3fd7c8ce35e88305e460b198b9f1bc1
Wendy Wang <wendywangg928@gmail.com>
2023-06-09 12:50:35 -0400

    added js to first and second page

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function Begin(e){
+const form = document.querySelector('form')
+const firstPage = document.getElementById('firstPage')
+const topicChoicesPage = document.getElementById('topicChoicePage')
+const quizPage = document.getElementById('quizPage')
+const choicelinks = document.querySelectorAll('.choicelink')
+
+function handleSubmit(e){
     e.preventDefault()
-    let name = document.forms['id']
+    let username = document.getElementById('PlayerNameInput').value 
+    localStorage.setItem('playerName',username)
+    firstPage.classList.add('hidden')
+    topicChoicesPage.classList.remove('hidden')
 }
+
+function startQuiz(e){
+    console.log(e.target)
+}
+
+for(let i = 0; i <choicelinks.length; i++){
+    choicelinks[i].addEventListener('click',startQuiz)
+}
+
+form.addEventListener('submit',handleSubmit)
+
